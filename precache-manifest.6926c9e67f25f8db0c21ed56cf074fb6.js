b47f52a19de28621e80758e9b778691475bab8bc
Rajat9915 <49584553+Rajat9915@users.noreply.github.com>
2020-05-20 14:43:08 +0530

    Updates

---
+++ precache-manifest.6926c9e67f25f8db0c21ed56cf074fb6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1a47b9ef1036165f9842d3812f615d9",
+    "url": "/project123/index.html"
+  },
+  {
+    "revision": "642942a314628a2cb6bc",
+    "url": "/project123/static/css/2.4c6ee7a4.chunk.css"
+  },
+  {
+    "revision": "43f5da3c8fed4ba7b20c",
+    "url": "/project123/static/css/main.453dba82.chunk.css"
+  },
+  {
+    "revision": "642942a314628a2cb6bc",
+    "url": "/project123/static/js/2.2d861fc7.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/project123/static/js/2.2d861fc7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "43f5da3c8fed4ba7b20c",
+    "url": "/project123/static/js/main.e4187cfa.chunk.js"
+  },
+  {
+    "revision": "a9574bc8cc7b720e6a1e",
+    "url": "/project123/static/js/runtime-main.6d8e3de2.js"
+  }
+]);
